3bf6cbe61160579a87bae0371e9daf7f8c975d2a
EnyaEithene <enya.eithene@gmail.com>
2025-06-01 15:03:56 +0300

    Add tab navigation keybinding

--- .config/lvim/lua/keymaps.lua
+++ .config/lvim/lua/keymaps.lua
@@ -5,6 +5,10 @@ lvim.keys.normal_mode["<C-j>"] = "<C-w>j"
 lvim.keys.normal_mode["<C-k>"] = "<C-w>k"
 lvim.keys.normal_mode["<C-l>"] = "<C-w>l"
 
+-- Navigate tabs
+vim.keymap.set("n", "<leader>h", ":tabprevious<CR>", { silent = true, desc = "Previous Tab" })
+vim.keymap.set("n", "<leader>l", ":tabnext<CR>", { silent = true, desc = "Next Tab" })
+
 -- Pandoc for markdown (WIP)
 lvim.keys.normal_mode["<leader>mp"] = function()
   vim.cmd("w") -- Save current buffer first
