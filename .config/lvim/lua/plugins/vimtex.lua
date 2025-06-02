3c7bbea686cd6a22355873f46a7dc1054d46d91e
EnyaEithene <enya.eithene@gmail.com>
2025-06-02 21:54:27 +0300

    Fix conceal option

--- .config/lvim/lua/plugins/vimtex.lua
+++ .config/lvim/lua/plugins/vimtex.lua
@@ -25,7 +25,35 @@ return {
         vim.g.vimtex_compiler_progname = "nvr"
 
         -- Completion mapping (\u a gets translated directly into .tex file)
-        vim.g.vimtex_imaps_enabled = 1
+        vim.g.tex_flavor = "latex"
+        vim.opt.conceallevel = 2
+        vim.opt.concealcursor = "nciv"  -- conceal in normal, command, insert, visual mode
+        
+        vim.opt.encoding = "utf-8"
+        vim.opt.fileencoding = "utf-8"
+        vim.cmd("syntax enable")
+        
+        -- Macros for tex files only
+        vim.api.nvim_create_autocmd("FileType", {
+          pattern = "tex",
+          callback = function()
+            -- Insert-mode mappings
+            vim.keymap.set("i", "<C-b>", "\\textbf{}<Left>", { buffer = true, desc = "Insert \\textbf{}" })
+            vim.keymap.set("i", "<C-i>", "\\textit{}<Left>", { buffer = true, desc = "Insert \\textit{}" })
+            vim.keymap.set("i", "<C-u>", "\\underline{}<Left>", { buffer = true, desc = "Insert \\underline{}" })
+            vim.keymap.set("i", "<C-m>", "$$<Left>", { buffer = true, desc = "Insert inline math" })
+
+            vim.keymap.set("i", "<A-[>", "\\u a", { buffer = true, noremap = true, desc = "Insert \\u a" })
+            vim.keymap.set("i", "<A-]>", "\\^ i", { buffer = true, noremap = true, desc = "Insert \\u a" })
+            vim.keymap.set("i", "<A-\\>", "\\^ a", { buffer = true, noremap = true, desc = "Insert \\u a" })
+            vim.keymap.set("i", "<A-;>", "\\c s", { buffer = true, noremap = true, desc = "Insert \\u a" })
+            vim.keymap.set("i", "<A-'>", "\\c t", { buffer = true, noremap = true, desc = "Insert \\u a" })
+
+            -- Visual-mode mappings
+            vim.keymap.set("v", "<C-b>", "<esc>`<i\\textbf{<esc>`>a}<esc>", { buffer = true, desc = "Wrap in \\textbf{}" })
+            vim.keymap.set("v", "<C-i>", "<esc>`<i\\textit{<esc>`>a}<esc>", { buffer = true, desc = "Wrap in \\textit{}" })
+          end,
+        })
       end,
     }
 }
